953e994efcbe2bd781f0e05a0c2633571eb6cd01
kpushkar10 <krish1234pk@gmail.com>
2021-05-11 14:11:33 +0530

    completed animation of delete/markDone

--- script.js
+++ script.js
@@ -156,15 +156,3 @@ document.querySelector('#next-year').onclick = () => {
     ++curr_year.value
     generateCalendar(curr_month.value, curr_year.value)
 }
-<<<<<<< HEAD
-
-let dark_mode_toggle = document.querySelector('.dark-mode-switch')
-
-dark_mode_toggle.onclick = () => {
-    document.querySelector('body').classList.toggle('light')
-    document.querySelector('body').classList.toggle('dark')
-}
-
-
-=======
->>>>>>> 22b2ecfe63ddb832071f23eaccc5b40d5b804c72
